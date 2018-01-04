a9afa7e010b536e11db9cba570fb61789b396cf7
Deyl <soulvisman@gmail.com>
2018-01-04 19:20:01 +0600

    one more example with Promise

--- playground/promise.js
+++ playground/promise.js
@@ -1,3 +1,25 @@
+const incrementAll = (arr) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      for (let i = 0; i < arr.length; i++){
+        if (typeof arr[i] == 'number'){
+          arr[i]++;
+        } else {
+          reject('Array has non-number element(s)!');
+        }
+      }
+      resolve(arr);
+    }, 2000);
+  });
+};
+
+incrementAll([2, 5, 1, 6, 'k', 10, 3])
+  .then((modifiedArr) => {
+    console.log(modifiedArr);
+  })
+  .catch((err) => console.log('Error:', err));
+
+/*
 const a = 10;
 const b = 5;
 
@@ -10,3 +32,4 @@ let handler = new Promise((resolve, reject) => {
 handler
   .then(result => console.log(result))
   .catch(error => console.log(error));
+*/
