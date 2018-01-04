2083049c5b7d1890a8ed6be460138121e2b4b59d
Deyl <soulvisman@gmail.com>
2018-01-04 15:57:48 +0600

    getting from console

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const request = require('request');
+const yargs = require('yargs');
 
-let url = 'https://maps.googleapis.com/maps/api/geocode/json?address=1301+lombard+st+philadelphia&key=';
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Type your adress',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+const address = encodeURIComponent(argv.address);
+
+let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=`;
 url = url + require('./apiKey');
 
 request({url, json:true}, (err, res, body) => {
   if (!err && res.statusCode == 200){
-    console.log('Adresss:', body.results[0].formatted_address);
+    console.log('Adress:', body.results[0].formatted_address);
     console.log('Latitude:', body.results[0].geometry.location.lat);
     console.log('Longitude:', body.results[0].geometry.location.lng);
   }
